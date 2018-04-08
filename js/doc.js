84e72a60a081f5b325106d35fd1d54b19072aab6
pageturners <38110311+pageturners@users.noreply.github.com>
2018-04-08 18:05:03 -0400

    Testing initialization

--- js/doc.js
+++ js/doc.js
@@ -1,13 +1,48 @@
+function getFileFromServer(url, doneCallback) {
+    var xhr;
 
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleStateChange;
+    xhr.open("GET", url, true);
+    xhr.send();
 
-function get_doc_order() {
-
+    function handleStateChange() {
+        if (xhr.readyState === 4) {
+            doneCallback(xhr.status == 200 ? xhr.responseText : null);
+        }
+    }
 }
 
-function init_doc() {
-
+function init_doc_view() {
+  getFileFromServer("data/1101163575072.txt", function(text) {
+    if (text === null) {
+        // An error occurred
+    }
+    else {
+        // `text` is the file text; set text to appear in div
+      document.getElementById("documentView").innerHTML = text
+    }
+  });
 }
 
-function next_doc() {
+// function init_doc_list() {
+// //fill second tab with list of documents, their relevance score & number of times they've been read
+// }
 
-}
+// function get_next_doc(currentFile) {
+// //return filename of next best document to read
+//   return filename
+// }
+
+// function next_doc() {
+//   filename = get_next_doc(currentDoc)
+//   getFileFromServer("data/"+filename, function(text) {
+//     if (text === null) {
+//         // An error occurred
+//     }
+//     else {
+//         // `text` is the file text
+//     }
+//   });
+// }
+
