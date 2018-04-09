92de418c638e153b4caed428a7dd71013fa5e1ec
pageturners <38110311+pageturners@users.noreply.github.com>
2018-04-09 12:47:35 -0400

    Fix line break issue

--- js/doc.js
+++ js/doc.js
@@ -8,6 +8,7 @@ function init_doc_view() {
             throw error;
         } else {
 //             console.log(text);
+		text = text.replace(/(?:\r\n|\r|\n)/g, '<br />');
             document.getElementById("document_view").innerHTML = text;          
         }
     });
