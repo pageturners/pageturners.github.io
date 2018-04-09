08135373eafa0812122f7401fc90d50c7be7e6e3
emilywall <emilywall@gatech.edu>
2018-04-09 15:32:35 -0400

    working on adding sliders to entities

--- js/entity.js
+++ js/entity.js
@@ -7,9 +7,9 @@ function entity_init() {
 	c1_table += '  <tr><th colspan="3">' + entity_types[0] + '</th></tr>';
 	for (var i = 0; i < person_entities.length; i++) {
 		c1_table += '<tr>';
-		c1_table += '  <td><input id="' + person_entities[i] + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>'; // placeholder for checkbox
-		c1_table += '  <td>' + person_entities[i] + '</td>';
-		c1_table += '  <td>**</td>'; // placeholder for weight slider
+		c1_table += '  <td class="checkbox_td"><input id="' + unformat_name(person_entities[i]) + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>'; 
+		c1_table += '  <td class="entity_td">' + person_entities[i] + '</td>';
+		c1_table += '  <td class="slider_td"><input id="' + unformat_name(person_entities[i]) + '_weight" type="range" min="0" max="100" value="100" class="slider"></td>'; 
 		c1_table += '</tr>';
 	}
 	c1_table += '</table>';
@@ -21,9 +21,9 @@ function entity_init() {
 	c2_table += '  <tr><th colspan="3">' + entity_types[1] + '</th></tr>';
 	for (var i = 0; i < organization_entities.length; i++) {
 		c2_table += '<tr>';
-		c2_table += '  <td><input id="' + organization_entities[i] + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>'; // placeholder for checkbox
-		c2_table += '  <td>' + organization_entities[i] + '</td>';
-		c2_table += '  <td>**</td>'; // placeholder for weight slider
+		c2_table += '  <td class="checkbox_td"><input id="' + unformat_name(organization_entities[i]) + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>';
+		c2_table += '  <td class="entity_td">' + organization_entities[i] + '</td>';
+		c2_table += '  <td class="slider_td"><input id="' + unformat_name(organization_entities[i]) + '_weight" type="range" min="0" max="100" value="100" class="slider"></td>';
 		c2_table += '</tr>';
 	}
 	c2_table += '</table>';
@@ -35,11 +35,18 @@ function entity_init() {
 	c3_table += '  <tr><th colspan="3">' + entity_types[2] + '</th></tr>';
 	for (var i = 0; i < location_entities.length; i++) {
 		c3_table += '<tr>';
-		c3_table += '  <td><input id="' + location_entities[i] + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>'; // placeholder for checkbox
-		c3_table += '  <td>' + location_entities[i] + '</td>';
-		c3_table += '  <td>**</td>'; // placeholder for weight slider
+		c3_table += '  <td class="checkbox_td"><input id="' + unformat_name(location_entities[i]) + '_checkBox" type="checkbox" class="entity_checkbox" checked/></td>'; 
+		c3_table += '  <td class="entity_td">' + location_entities[i] + '</td>';
+		c3_table += '  <td class="slider_td"><input id="' + unformat_name(location_entities[i]) + '_weight" type="range" min="0" max="100" value="100" class="slider"></td>';
 		c3_table += '</tr>';
 	}
 	c3_table += '</table>';
 	c3_div.innerHTML = c3_table;
+}
+
+function unformat_name(name) {
+	var new_name = name.replace(/[.,\/#!$%\^&\*;:{}=\-_`'~()]/g,"");
+	new_name = new_name.replace(/\s{2,}/g," ");
+	new_name = new_name.replace(' ', '');
+	return new_name;
 }
