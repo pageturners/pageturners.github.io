8ddb75940a7e9b09d6d396d7b2d5735a44a36ae1
Steve-Jones <steve@stevejones.io>
2018-04-09 02:34:56 -0400

    Cleaned up HTML/CSS structure & threw in a placeholder d3 timeline

--- js/main.js
+++ js/main.js
@@ -8,8 +8,92 @@ function init() {
 		entity_type_map[data['entity_name']] = { 'type': data['entity_type'], 'total_appearances': data['total_appearances'], 'document_appearances': data['document_appearances'] };
 	});
 
-	// wait one second to make sure data finishes loading 
+	// wait one second to make sure data finishes loading
 	window.setTimeout(function() {
 		entity_init();
 	}, 1000);
+
+	// Timeline
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	    width = 1280 - margin.left - margin.right,
+	    height = 330 - margin.top - margin.bottom;
+
+	var x = d3.scale.linear()
+	    .range([0, width]);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var color = d3.scale.category10();
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	var svg = d3.select("#timeline").append("svg")
+	    .attr("width", width + margin.left + margin.right1000)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.tsv("data.tsv", function(error, data) {
+	  if (error) throw error;
+
+	  data.forEach(function(d) {
+	    d.sepalLength = +d.sepalLength;
+	    d.sepalWidth = +d.sepalWidth;
+	  });
+
+	  x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
+	  y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("class", "label")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Relavance")
+
+	  svg.selectAll(".dot")
+	      .data(data)
+	    .enter().append("circle")
+	      .attr("class", "dot")
+	      .attr("r", 3.5)
+	      .attr("cx", function(d) { return x(d.sepalWidth); })
+	      .attr("cy", function(d) { return y(d.sepalLength); })
+	      .style("fill", function(d) { return color(d.species); });
+
+	  var legend = svg.selectAll(".legend")
+	      .data(color.domain())
+	    .enter().append("g")
+	      .attr("class", "legend")
+	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	  legend.append("rect")
+	      .attr("x", width - 18)
+	      .attr("width", 18)
+	      .attr("height", 18)
+	      .style("fill", color);
+
+	  legend.append("text")
+	      .attr("x", width - 24)
+	      .attr("y", 9)
+	      .attr("dy", ".35em")
+	      .style("text-anchor", "end")
+	      .text(function(d) { return d; });
+
+	});
 }
