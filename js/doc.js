005fd075756315c81407080759439074bd2e9978
pageturners <38110311+pageturners@users.noreply.github.com>
2018-04-10 23:11:44 -0400

    Atricle map

--- js/doc.js
+++ js/doc.js
@@ -15,9 +15,33 @@ function doc_init() {
 
 	document.getElementById("defaultOpen").click();
 
-    	var document_list_table = document.getElementById('document_list');
-	var dl_header = '<tr>';
-	var document_list = {};
+    	// create a table for the first entity type
+// 	var doc_div = document.getElementById('doc_table');
+// 	var doc_table_header = '<table id="doc_table_header" class="doc_header">'; 
+// 	doc_table_header += '  <tr><th>Filename</th><th>Type</th><th>Date</th><th>Title</th><th>Author</th><th>Views</th><th>Relevance</th></tr>';
+// 	doc_table_header += '</table>';
+// 	var doc_table = '<table id="doc_table" class="doc_table">';
+	x = 0
+	for (var key in article_map) {
+// 		doc_table += '<tr>';
+// 		doc_table += '  <td class="doc_td">' + key + '</td>'; //Filenames
+		var values = article_map[key]
+		if (x == 0) {
+			console.log(values);
+		}
+		x += 1;
+// 		doc_table += '  <td class="doc_td">' + person_entities[i] + '</td>'; //type
+// 		doc_table += '  <td class="doc_td">' + person_entities[i] + '</td>'; //date
+// 		doc_table += '  <td class="doc_td">' + person_entities[i] + '</td>'; //title
+// 		doc_table += '  <td class="doc_td">' + person_entities[i] + '</td>'; //author
+// 		doc_table += '  <td class="doc_td">0</td>'; //view count
+// 		doc_table += '  <td class="doc_td">Rel.</td>'; //relevance
+// 		doc_table += '</tr>';
+	}
+
+// 	doc_table += '</table>';
+// 	doc_div.innerHTML = doc_table_header + doc_table;
+
 	
     //create table from articleInfo.csv and add columns for number of times read and relevance score
 }
