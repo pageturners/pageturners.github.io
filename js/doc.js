fd247423bd8211f50788c4b713d80a3b5f40550c
pageturners <38110311+pageturners@users.noreply.github.com>
2018-04-12 12:17:27 -0400

    Highlight test

--- js/doc.js
+++ js/doc.js
@@ -86,9 +86,11 @@ function openDoc(filename) {
 // }
 
 function highlight(text) {
+	console.log('text to highlight:'+text);
   var inputText = document.getElementById("document_view");
   var innerHTML = inputText.innerHTML;
   var index = innerHTML.indexOf(text);
+	console.log('index to highlight:'+index)
   if (index >= 0) { 
    innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
    inputText.innerHTML = innerHTML;
