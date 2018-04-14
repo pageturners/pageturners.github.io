3570cdcaa6142801344e08046af8c91418a4a2ab
pageturners <38110311+pageturners@users.noreply.github.com>
2018-04-13 20:54:57 -0400

    testing identifying clicked dots

--- js/timeline.js
+++ js/timeline.js
@@ -62,12 +62,21 @@ function timeline_init() {
 
 	svg.selectAll(".dot")
 		.data(all_articles)
-	  .enter().append("circle")
+	  	.enter().append("circle")
 		.attr("class", "dot")
 		.attr("r", 3.5)
 		.attr("cx", function(d) { console.log(time_format.parse(article_map[d]['date'])); return x(time_format.parse(article_map[d]['date'])); })
 		.attr("cy", function(d) { return y(article_weight_map[d]); })
+		.attr("id", function(d) { console.log('filenames from graph: '+article_map[d]['filename'].slug); return article_map[d]['filename'].slug; });
 		.style("fill", "#000000");
+		.on('click', function(d,i) {
+            // handle events here
+            // d - datum
+            // i - identifier or index
+            // this - the `<rect>` that was clicked
+			console.log('i: '+i+', d: '+d);
+			//openDoc(i);
+        	});
 }
 
 // todo: update stuff in the timeline (called after document scores change)
